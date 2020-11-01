7fec231f558ba034ca3c9f1acf56023fc8b0667c
PagonisTasos <apagon@protonmail.com>
2020-11-01 03:00:23 +0200

    string - null if empty

---
+++ Odysseus/System.String/String.NullIfEmpty.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+public static partial class Extensions
+{
+    /// <summary>
+    ///     Returns null if string is empty.
+    /// </summary>
+    /// <param name="this">The @this to act on.</param>
+    /// <returns>This string or null if string is empty.</returns>
+    public static string NullIfEmpty(this String @this)
+    {
+        return String.IsNullOrEmpty(@this) ? null : @this;
+    }
+}
