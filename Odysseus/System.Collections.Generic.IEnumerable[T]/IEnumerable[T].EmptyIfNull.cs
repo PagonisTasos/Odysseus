ed4900f85aea6d2682207093ad3f9ef6be62cf8e
PagonisTasos <apagon@protonmail.com>
2020-11-01 03:31:00 +0200

    ienumerable - empty if null

---
+++ Odysseus/System.Collections.Generic.IEnumerable[T]/IEnumerable[T].EmptyIfNull.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+public static partial class Extensions
+{
+    /// <summary>
+    ///     Returns empty collection if null.
+    /// </summary>
+    /// <typeparam name="T">Generic type parameter.</typeparam>
+    /// <param name="this">The collection to act on.</param>
+    /// <returns>the collection or empty collection if null.</returns>
+    public static IEnumerable<T> EmptyIfNull<T>(this IEnumerable<T> @this)
+    {
+        return @this ?? Enumerable.Empty<T>();
+    }
+}
