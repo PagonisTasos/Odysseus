0eec63964fecc10e4ea0508a85e9e9efaa4b0996
PagonisTasos <apagon@protonmail.com>
2020-11-13 23:29:11 +0200

    controller context

---
+++ Odysseus/System.Web.Mvc.ControllerContext/ControllerContext.ActionResultToString.cs
@@ -0,0 +1,37 @@
+﻿using System.Web;
+using System.Web.Mvc;
+
+public static partial class Extensions
+{
+    public static string RenderViewToString(this ControllerContext @this, ActionResult actionResult)
+    {
+        // Create memory writer.
+        var sb = new System.Text.StringBuilder();
+        var memWriter = new System.IO.StringWriter(sb);
+
+        // Create fake http context to render the view.
+        var fakeResponse = new HttpResponse(memWriter);
+        var fakeContext = new HttpContext(
+            HttpContext.Current.Request,
+            fakeResponse
+            );
+        var fakeControllerContext = new ControllerContext(
+            new HttpContextWrapper(fakeContext),
+            @this.RouteData,
+            @this.Controller
+            );
+        var oldContext = HttpContext.Current;
+        HttpContext.Current = fakeContext;
+
+        // Render the view.
+        actionResult.ExecuteResult(fakeControllerContext);
+
+        // Restore old context.
+        HttpContext.Current = oldContext;
+
+        // Flush memory and return output.
+        memWriter.Flush();
+        return sb.ToString();
+
+    }
+}
